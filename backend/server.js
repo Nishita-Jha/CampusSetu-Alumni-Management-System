08b080d567ef1cd68442313ccfa7df89ef46fe1d
Nishita-Jha <nishitajha2906@gmail.com>
2026-04-08 13:25:23 +0530

    updated app.get in server.js

--- backend/server.js
+++ backend/server.js
@@ -62,6 +62,9 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("🚀 CampusSetu Backend is Running Successfully!");
+});
 
 // ✅ Serve uploaded images statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
