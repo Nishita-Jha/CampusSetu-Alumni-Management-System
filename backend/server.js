90e6a33bb8a33939ac388083b8a55c9d59c1d5ae
Nishita-Jha <nishitajha2906@gmail.com>
2026-04-08 14:08:07 +0530

    updated / in app.get in server.js

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.use("/api/mentors", mentorRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Catch-all for React routing
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
